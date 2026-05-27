25c419029d1a8bb73946adc2c9a314a9fc395b10
Disha407 <dhallshashank@gmail.com>
2026-05-27 15:42:27 +0530

    lesson 2

---
+++ lesson 2/activity1.py
@@ -0,0 +1,5 @@
+print ("hello world")
+print(15)
+print("hello world\n")
+print("hello",9)
+    
---
+++ lesson 2/activity2.py
@@ -0,0 +1,6 @@
+x=5
+y="disha"
+print(x)
+print(y)
+name=input("enter your name")
+print("hello",name,"welcome")
