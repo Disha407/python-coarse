d0be24015bdbc2a0bf2457f9703a2d193ee87ccd
Disha407 <dhallshashank@gmail.com>
2026-06-10 15:51:58 +0530

    summertimes.py

---
+++ homework/summertimes.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter the temperature: "))
+
+if temperature >= 25:
+    print("You can wear light and soft clothes.")
+elif temperature >= 15:
+    print("Wear a jacket")
+else:
+    print("It is cold. Wear warm clothes.")
