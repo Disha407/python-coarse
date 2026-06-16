7744a4749e757aa353f91dee376869d70949ea84
Disha407 <dhallshashank@gmail.com>
2026-06-16 15:40:45 +0530

    lesson7

---
+++ lesson 7/activity2.py
@@ -0,0 +1,20 @@
+# 1) Store 10 in `a`
+a=10
+# 2) Perform a right shift on `a` by 1 and print the result.
+
+# (Right shift means moving all bits one place to the right.)
+
+#
+
+# 3)print("a >> 1 =", a >> 1)
+
+print("a>>1=",a>>1)
+
+
+
+# 4) Change `a` to 5
+a=5
+# 5) Perform a left shift on `a` by 1 and print the result.
+print("a<<1=",a<<1)
+# (Left shift means moving all bits one place to the left.)
+
